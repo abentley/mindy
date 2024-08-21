6819a99d9471e223d9c9d151744ec9f384580f7f
Aaron Bentley <aaron@aaronbentley.com>
2024-08-20 22:52:21 -0400

    Implement writing messages in a loop.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::fs::File;
+use std::thread::sleep;
+use std::time::Duration;
+use daemonize::Daemonize;
+
 fn main() {
     println!("Hello, world!");
+    let stdout = File::create("stdout-1").unwrap();
+    let Ok(daemon) = Daemonize::new().stdout(stdout).start().unwrap();
+    loop {
+        println!("Hello underworld!");
+        sleep(Duration::from_secs(1));
+    }
 }
